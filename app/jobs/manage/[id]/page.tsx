766a7671fdc9650089ea9906e2e56cf517055ca7
jaretlewis007-blip <jaretlewis007@gmail.com>
2025-12-06 20:35:02 -0600

    Update page.tsx

--- app/jobs/manage/[id]/page.tsx
+++ app/jobs/manage/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { db } from "@/firebase/config";
+import { db } from "../../../firebase/config";
 import { doc, getDoc } from "firebase/firestore";
 
 export default function ManageJobPage() {
@@ -34,3 +34,4 @@ export default function ManageJobPage() {
     </div>
   );
 }
+
